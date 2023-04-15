ada8232e949c1c718f77505c29821daa0b65cbd8
Dongyeb Choi <dongyub1015@gmail.com>
2023-04-15 22:16:25 +0900

    Add api sample

--- src/server/hello.py
+++ src/server/hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/api')
+def get_api():
+    return {'hello': 'world'}
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
